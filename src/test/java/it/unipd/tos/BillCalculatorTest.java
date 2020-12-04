228166d10e2a6ab4da2e453be6c9054aa0cbf0e1
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2020-12-04 23:45:27 +0100

    test per sconto 50% gelato meno costoso se ordinati più di 5 Gelati

--- src/test/java/it/unipd/tos/BillCalculatorTest.java
+++ src/test/java/it/unipd/tos/BillCalculatorTest.java
@@ -38,4 +38,23 @@ public class BillCalculatorTest {
   public void denyNullOrder() throws IllegalArgumentException, TakeAwayBillException {
     calculator.getOrderPrice(null);
   }
+
+  @Test
+  public void testForBasicOrderWithMoreThan5IceCreams() throws TakeAwayBillException {
+    var items = List.of(
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 1", 10d),
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 2", 7d),
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 3", 7.7d),
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 4", 4d),
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 5", 2d),
+            new MenuItem(MenuItem.ItemType.GELATO, "gelato 6", 1d),
+            new MenuItem(MenuItem.ItemType.BUDINO, "budino 1", 5.5d),
+            new MenuItem(MenuItem.ItemType.BEVANDA, "bevanda 1", 3d)
+    );
+    Order order = new Order(items, 19, 10, new User("Test", "Test", 20));
+    assertEquals(
+            40.2 - 0.5, // totale - 50% gelato meno costoso
+            calculator.getOrderPrice(order),
+            DELTA);
+  }
 }
